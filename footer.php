c70972c2f87c28b9f18fb8e5fb30131a9a46abe4
Pallashree <73779573+Pallashree@users.noreply.github.com>
2020-11-09 19:57:07 +0530

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,6 @@
+<footer>
+	
+		Copyright © Lifestyle Store. All Rights
+Reserved” and “Contact Us: +91 90000 00000
+	
+</footer>
---
+++ index.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+
+    <link rel="stylesheet" type="text/css" href="style.css">
+
+</head>
+
+<body>
+
+	<nav class="navbar navbar-inverse navbar-fixed-top">
+		<div class="container">
+			
+			<div class="navbar-header">
+				<button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+                    
+				</button>
+				
+                <a href="#" class="navbar-brand">Lifestyle Store</a><span class="gylphicon gylphicon-user">  
+
+			</div>
+			<div>
+				<div class="collaspe navbar-collaspe" id="myNavbar">
+			
+                <ul class="nav navbar-nav navbar-right">
+					<li><a href="signup.html"><span class="glyphicon glyphicon-user"></span> SignUp</a></li>
+					<li><a href="login.html"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
+                    
+                </ul>
+            
+
+			</div>
+
+		</div>
+		
+
+
+
+	</nav>
+
+   <div id=banner_image>
+   	<div class="container">
+   	<!--img src="intro-bg_1.jpg" style="background: no-repeat center center;"-->
+   	     <div id="banner_content">
+   	     	<a href="product.html" class="btn btn-danger btn-lg active">Shop Now</a>
+   	     </div>
+   	</div>
+   	
+   </div>
+
+
+
+
+</body>
+<?php include="footer.php" ?>
+</html>
